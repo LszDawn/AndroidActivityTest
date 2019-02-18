6bfcd7eea48dea96d00f97194c92af5adac67222
罗思之 <sizhi.luo@bangdao-tech.com>
2019-02-18 21:13:14 +0800

    定义了菜单

--- app/src/main/java/com/example/lsz/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/lsz/activitytest/FirstActivity.java
@@ -2,6 +2,8 @@ package com.example.lsz.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -24,5 +26,30 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.makeText(FirstActivity.this, "hey man", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    //重写onCreateOptionsMenu方法，让菜单显示出来
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //获取MenuInflater对象并调用其inflate方法：参数1指定资源文件,即main.xml.参数2指定添加到哪个menu对象中
+        getMenuInflater().inflate(R.menu.main, menu);
+        //true表示允许创建的订单显示出来
+        return true;
+    }
+
+    //重写onOptionsItemSelected方法，让菜单能响应事件
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this, "你按了添加", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "你按了移除", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }

399cd6f2886f697ef22f386427a72464d837d463
罗思之 <sizhi.luo@bangdao-tech.com>
2019-02-19 15:20:29 +0800

    如何规范的启动activity

--- app/src/main/java/com/example/lsz/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/lsz/activitytest/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.lsz.activitytest;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class SecondActivity extends BaseActivity {
+
+
+    /**
+     * 让启动SecondActivity的参数一目了然
+     * @param context
+     * @param data1
+     * @param data2
+     */
+    public static void actionStart(Context context, String data1, String data2){
+        Intent intent = new Intent(context, SecondActivity.class);
+        intent.putExtra("param1", data1);
+        intent.putExtra("param2", data2);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
